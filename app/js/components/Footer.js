b88dff92e0bc1bc53628aafc803ed986fd15eb23
JaberQayad <jaberqayad@gmail.com>
2025-12-28 21:23:04 +0100

    update footer

--- app/js/components/Footer.js
+++ app/js/components/Footer.js
@@ -1,7 +1,8 @@
 export const Footer = (settings) => `
     <footer class="app-footer" ${settings.footerColor ? `style="color: ${settings.footerColor}"` : ''}>
         <div class="footer-content">
-            <span>${settings.footerText || 'Powered by'}</span> <a href="https://github.com/JaberQayad/dashlet" target="_blank">Dashlet</a>
+            <span>${settings.footerText || 'Powered by'} </span>
+            <a href="https://github.com/JaberQayad/dashlet" target="_blank">Dashlet</a>
         </div>
     </footer>
 `;
